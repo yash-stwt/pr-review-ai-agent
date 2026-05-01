0d445018a52a4cba031c25378dd070dea6e6d5f9
Yash Singh <yashsingh7606@gmail.com>
2026-05-01 15:52:42 +0530

    cors issue fix

--- src/main/java/com/aicodereviewer/backend/config/CorsConfig.java
+++ src/main/java/com/aicodereviewer/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","https://pr-review-ai-agent-pghm.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
